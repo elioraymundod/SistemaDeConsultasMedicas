3589cf7c0b087cfb2f2f85eebdb1d16d7de4b0a4
Elio Raymundo <raymundoelio@gmail.com>
2021-03-03 21:19:26 -0600

    Se agregan scripts para creacion de base de datos e insercion de datos

---
+++ Scripts DB/CreacionDB.sql
@@ -0,0 +1,140 @@
+CREATE DATABASE muestras_medicas_db;
+
+CREATE TABLE `catalogos` (
+  `codigo_catalogo` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(250) NOT NULL,
+  `descripcion` varchar(250) NOT NULL,
+  `fecha_creacion` date NOT NULL,
+  `usuario_creacion` varchar(50) NOT NULL,
+  `ip_usuario_creacion` varchar(20) NOT NULL,
+  `fecha_modificacion` date NULL,
+  `usuario_modificacion` varchar(50) NULL,
+  `ip_usuario_modificacion` varchar(20) NULL,
+  PRIMARY KEY (`codigo_catalogo`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 ;
+
+CREATE TABLE `datos_catalogos` (
+  `codigo_dato_catalogo` int NOT NULL AUTO_INCREMENT,
+  `codigo_catalogo` int NOT NULL,
+  `nombre` varchar(250) NOT NULL,
+  `descripcion` varchar(250) NOT NULL,
+  `fecha_creacion` date NOT NULL,
+  `usuario_creacion` varchar(50) NOT NULL,
+  `ip_usuario_creacion` varchar(20) NOT NULL,
+  `fecha_modificacion` date NULL,
+  `usuario_modificacion` varchar(50) NULL,
+  `ip_usuario_modificacion` varchar(20) NULL,
+  PRIMARY KEY (`codigo_dato_catalogo`),
+  CONSTRAINT `codigo_catalogo_a` FOREIGN KEY (`codigo_catalogo`) REFERENCES `catalogos` (`codigo_catalogo`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 ;
+
+
+CREATE TABLE `usuarios` (
+  `nit_usuario` varchar(11) NOT NULL,
+  `codigo_rol` int NOT NULL,
+  `nombre_usuario` varchar(100) NOT NULL,
+  `user_usuario` varchar(50) NOT NULL,
+  `password_usuario` varchar(100) NOT NULL,
+  `fecha_creacion` date NOT NULL,
+  `usuario_creacion` varchar(50) NOT NULL,
+  `ip_usuario_creacion` varchar(20) NOT NULL,
+  `fecha_modificacion` date NULL,
+  `usuario_modificacion` varchar(50) NULL,
+  `ip_usuario_modificacion` varchar(20) NULL,
+  PRIMARY KEY (`nit_usuario`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+
+CREATE TABLE `clientes` (
+  `nit_cliente` varchar(11) NOT NULL,
+  `cdigo_tipo_solicitante` int NOT NULL,
+  `nombre_cliente` varchar(100) NOT NULL,
+  `direccion_cliente` varchar(100) NOT NULL,
+  `telefonos` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `fecha_creacion` date NOT NULL,
+  `usuario_creacion` varchar(50) NOT NULL,
+  `ip_usuario_creacion` varchar(20) NOT NULL,
+  `fecha_modificacion` date NULL,
+  `usuario_modificacion` varchar(50) NULL,
+  `ip_usuario_modificacion` varchar(20) NULL,
+  PRIMARY KEY (`nit_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+
+CREATE TABLE `expedientes` (
+  `no_expediente` varchar(21) NOT NULL,
+  `nit_cliente` varchar(11) NOT NULL,
+  `observaciones` varchar(100) NOT NULL,
+  `fecha_creacion` date NOT NULL,
+  `usuario_creacion` varchar(50) NOT NULL,
+  `ip_usuario_creacion` varchar(20) NOT NULL,
+  `fecha_modificacion` date NULL,
+  `usuario_modificacion` varchar(50) NULL,
+  `ip_usuario_modificacion` varchar(20) NULL,
+  PRIMARY KEY (`no_expediente`),
+  CONSTRAINT `nit_cliente_a` FOREIGN KEY (`nit_cliente`) REFERENCES `clientes` (`nit_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+
+CREATE TABLE `muestras` (
+  `codigo_muestra` varchar(17) NOT NULL,
+  `codigo_tipo_muestra` int NOT NULL,
+  `unidad_medica` int NOT NULL,
+  `presentacion` varchar(2000) NOT NULL, 
+  `cantidadUnidades` int NOT NULL, 
+  `adjunto` blob null,
+  `fecha_vencimiento` date NOT NULL,
+  `fecha_creacion` date NOT NULL,
+  `usuario_creacion` varchar(50) NOT NULL,
+  `ip_usuario_creacion` varchar(20) NOT NULL,
+  `fecha_modificacion` date NULL,
+  `usuario_modificacion` varchar(50) NULL,
+  `ip_usuario_modificacion` varchar(20) NULL,
+  PRIMARY KEY (`codigo_muestra`)
+  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+
+CREATE TABLE `solicitudes_de_muestras` (
+  `codigo_solicitud` varchar(17) NOT NULL,
+  `codigo_tipo_solicitante` int NOT NULL,
+  `codigo_tipo_solicitud` int NOT NULL,
+  `no_expediente` varchar(21) NOT NULL,
+  `codigo_tipo_soporte` int NOT NULL,
+  `codigo_estado` int NOT NULL,  
+  `usuario_asignacion` varchar(11),  
+  `no_soporte` varchar(50) NOT NULL,
+  `nit` varchar(11),  
+  `cantidad_de_muestras` int,  
+  `dias_de_items` int,
+  `dias_vencimiento` int,
+  `descripcion` varchar(2000),
+  `telefonos` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `fecha_creacion` date NOT NULL,
+  `usuario_creacion` varchar(50) NOT NULL,
+  `ip_usuario_creacion` varchar(20) NOT NULL,
+  `fecha_modificacion` date NULL,
+  `usuario_modificacion` varchar(50) NULL,
+  `ip_usuario_modificacion` varchar(20) NULL,
+  PRIMARY KEY (`codigo_solicitud`),
+  CONSTRAINT `no_expediente_a` FOREIGN KEY (`no_expediente`) REFERENCES `expedientes` (`no_expediente`),
+  CONSTRAINT `usuario_asignacion_a` FOREIGN KEY (`usuario_asignacion`) REFERENCES `usuarios` (`nit_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+
+
+CREATE TABLE `etiqueta_de_muestra` (
+  `codigo_etiqueta` int NOT NULL AUTO_INCREMENT,
+  `codigo_muestra` varchar(17) NOT NULL,
+  `codigo_solicitud` varchar(17) NOT NULL,
+  `no_expediente` varchar(21) NOT NULL,
+  `codigo_qr` varchar(34) NOT NULL,
+  `descripcion` varchar(500) NOT NULL,  
+  `fecha_creacion` date NOT NULL,
+  `usuario_creacion` varchar(50) NOT NULL,
+  `ip_usuario_creacion` varchar(20) NOT NULL,
+  `fecha_modificacion` date NULL,
+  `usuario_modificacion` varchar(50) NULL,
+  `ip_usuario_modificacion` varchar(20) NULL,
+  PRIMARY KEY (`codigo_etiqueta`),
+  CONSTRAINT `no_expediente_fk` FOREIGN KEY (`no_expediente`) REFERENCES `expedientes` (`no_expediente`),
+  CONSTRAINT `codigo_solicitud_fk` FOREIGN KEY (`codigo_solicitud`) REFERENCES `solicitudes_de_muestras` (`codigo_solicitud`),
+  CONSTRAINT `codigo_muestra_fk` FOREIGN KEY (`codigo_muestra`) REFERENCES `muestras` (`codigo_muestra`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
---
+++ Scripts DB/InsertData.sql
@@ -0,0 +1,58 @@
+INSERT INTO `catalogos` VALUES (1, 'Tipo Muestra', 'Tipos de muestras medicas.', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+    							(2, 'Unidad Medica', 'Unidades medicas.', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                (3, 'Estados Solicitud', 'Estados en los que puede estar una solicitud', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                (4, 'Tipo Soporte', 'Tipos de soporte que puede tener una solicitud.', '2021-03-03', 'master', '0.0.0.0', null, '',''),
+                                (5, 'Tipo solicitud', 'Tipos que puede ser una soliciud', '2021-03-03', 'master', '0.0.0.0', null, '',''),
+                                (6, 'Tipo solicitante', 'Tipos de solicitantes que pueden crear una solicitud.', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+								(7, 'Tipo solicitante', 'Tipos de solicitantes que pueden crear una solicitud.', '2021-03-03', 'master', '0.0.0.0', null, '', '');
+
+INSERT INTO `datos_catalogos` VALUES (1, 1, 'Cultivo', 'Cultivo', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (2, 1, 'Plaquetas', 'Plaquetas', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (3, 1, 'Eses', 'Eses', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (4, 1, 'Orina', 'Orina', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (5, 2, 'Mililitros', 'Mililitros', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (6, 2, 'Gramos', 'Gramos', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (7, 2, 'Miligramos', 'Miligramos', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (8, 3, 'Creado', 'Creado', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (9, 3, 'Enviado', 'Enviado', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (10, 3, 'Asignado', 'Asignado', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (11, 3, 'Analisis', 'Analisis', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (12, 3, 'Revision', 'Revision', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (13, 3, 'Espera', 'Espera', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (14, 3, 'Rechazado', 'Rechazado', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (15, 3, 'Autorizado', 'Autorizado', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (16, 3, 'Finalizado', 'Finalizado', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (17, 3, 'Eliminado', 'Eliminado', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (18, 4, 'SM-Solicitud Medica', 'SM-Solicitud Medica', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (19, 4, 'ET- Examén externo', 'ET- Examén externo', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (20, 4, 'FP-FCTURA', 'FP-FCTURA', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (21, 4, 'HO-Hoja de oficio', 'HO-Hoja de oficio', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (22, 5, 'MM-Muestra medica', 'MM-Muestra medica', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (23, 5, 'LQ-Laboratorio', 'LQ-Laboratorio', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (24, 6, 'IN-Usuario interno', 'IN-Usuario interno', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (25, 6, 'EX-Usuario externo', 'EX-Usuario externo', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (26, 7, 'rolUsuario', 'rolUsuario', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (27, 7, 'rolCentralizador', 'rolCentralizador', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (28, 7, 'rolAnalista', 'rolAnalista', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+                                        (29, 7, 'rolRevisor', 'rolRevisor', '2021-03-03', 'master', '0.0.0.0', null, '', ''),
+										(30, 7, 'rolAutorizador', 'rolAutorizador', '2021-03-03', 'master', '0.0.0.0', null, '', '');
+
+INSERT INTO `clientes` VALUES ('45185954845', 24, 'Jeissy Dariela Castellanos Hernandez', 'San Juan Sacatepequez', '12458495/65152458', 'dariela@gmail.com', '2021-03-03', 'master', '0.0.0.0', null, '', '');
+INSERT INTO `clientes` VALUES ('95854565123', 24, 'Juan Carlos Estrada Aguirre', 'San Raymundo', '45958584/52002556', 'juancarlos@gmail.com', '2021-03-03', 'master', '0.0.0.0', null, '', '');
+INSERT INTO `clientes` VALUES ('18545986565', 24, 'Martha Nineth Lopez Urizar', 'Lo de Mejia', '12348595', 'martha@gmail.com', '2021-03-03', 'master', '0.0.0.0', null, '', '');
+INSERT INTO `clientes` VALUES ('64584568489', 25, 'Pahola Marin Catalan', 'Zona 7, Guatemala', '45985875/78945858', 'pahola@gmail.com', '2021-03-03', 'master', '0.0.0.0', null, '', '');
+INSERT INTO `clientes` VALUES ('49548984954', 25, 'Rodolfo Bartolome Juarez Rojas', 'Zona 14', '52465859', 'rodolfo@gmail.com', '2021-03-03', 'master', '0.0.0.0', null, '', '');
+INSERT INTO `clientes` VALUES ('19584958756', 25, 'Mael Rosalio Rosales Rosas', 'Amatitlan', '49585946/95858485', 'mael@gmail.com', '2021-03-03', 'master', '0.0.0.0', null, '', '');
+
+INSERT INTO `expedientes` VALUES('4589-85-95-47-7845958', '45185954845', 'Expediente activo.', '2021-03-03', 'master', '0.0.0.0', null, '', '');
+INSERT INTO `expedientes` VALUES('9858-59-45-71-4565848', '95854565123', 'Expediente activo.', '2021-03-03', 'master', '0.0.0.0', null, '', '');
+INSERT INTO `expedientes` VALUES('9851-75-15-45-4598545', '18545986565', 'Expediente activo.', '2021-03-03', 'master', '0.0.0.0', null, '', '');
+INSERT INTO `expedientes` VALUES('8495-77-55-66-1234567', '64584568489', 'Expediente activo.', '2021-03-03', 'master', '0.0.0.0', null, '', '');
+INSERT INTO `expedientes` VALUES('4859-77-95-59-1595498', '49548984954', 'Expediente activo.', '2021-03-03', 'master', '0.0.0.0', null, '', '');
+INSERT INTO `expedientes` VALUES('7895-45-89-45-4958548', '19584958756', 'Expediente activo.', '2021-03-03', 'master', '0.0.0.0', null, '', '');
+
+INSERT INTO `usuarios` VALUES ('49548585958', 26, 'Elio Isai Raymundo Diaz', 'aeiraymun', '1234','2021-03-03', 'master', '0.0.0.0', null, '', '');
+INSERT INTO `usuarios` VALUES ('98456849845', 27, 'Lester Raul Toj', 'lrtoj', '1234','2021-03-03', 'master', '0.0.0.0', null, '', '');
+INSERT INTO `usuarios` VALUES ('97845688595', 28, 'Diego Sebastian Mijangos Flores', 'dsmijangos','1234','2021-03-03', 'master', '0.0.0.0', null, '', '');
+INSERT INTO `usuarios` VALUES ('98511544899', 29, 'Denis Ivan Santizo Pocon', 'disantizo', '1234','2021-03-03', 'master', '0.0.0.0', null, '', '');
+INSERT INTO `usuarios` VALUES ('44880459848', 30, 'Madeline Yocary Coronado ', 'mycoronado', '1234','2021-03-03', 'master', '0.0.0.0', null, '', '');
